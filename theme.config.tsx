78acbbbe363e0166f6fd1d612c78ace7af3b9be5
Pankratov <pnrf@yandex.ru>
2023-05-25 19:09:46 +0300

    ci: update theme.config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,34 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  // logo: <span>My Project</span>,
+	logo: (
+    <>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Myrepo.ru |
+      </span>
+			<span style={{ marginLeft: '.4em', fontWeight: 400, fontSize: '.9em' }}>
+        База знаний технического писателя
+      </span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/pnrf/tech-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // chat: {
+  //   link: 'https://discordapp.com/users/1058075588700672041',
+  // },
   docsRepositoryBase: 'https://github.com/pnrf/tech-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+        <p>{new Date().getFullYear()} ©{' Все права защищены. '}</p>
+        <a href="https://nextra.site" target="_blank">
+          Powered by Nextra
+        </a>
+        .
+      </span>
+    ),
   },
 }
 
