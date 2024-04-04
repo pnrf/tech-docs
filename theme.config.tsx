15a111b30e303aa0a1e393e933904c53a03e377b
Pankratov <pnrf@yandex.ru>
2024-04-04 10:56:25 +0300

    build: app new pages to navbar

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,13 +4,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
 	logo: (
     <>
-			<svg width="36px" height="36px" viewBox="0 0 91 91" enable-background="new 0 0 91 91" id="Layer_1" version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M88.587,25.783L46.581,5.826c-0.672-0.317-1.45-0.317-2.12,0L2.462,25.783 c-0.861,0.408-1.41,1.277-1.41,2.232c0,0.951,0.549,1.819,1.41,2.229l41.999,19.954c0.335,0.158,0.697,0.239,1.059,0.239 c0.363,0,0.726-0.081,1.061-0.239l42.006-19.954c0.861-0.41,1.41-1.278,1.41-2.229C89.997,27.06,89.448,26.191,88.587,25.783z" fill="#647F94"></path> <path d="M45.521,68.085c-0.483,0-0.965-0.105-1.414-0.317L2.109,47.813c-1.643-0.781-2.341-2.744-1.562-4.386 c0.78-1.642,2.742-2.341,4.388-1.562l40.584,19.283l40.595-19.283c1.639-0.78,3.606-0.083,4.386,1.562 c0.78,1.643,0.083,3.605-1.562,4.386L46.934,67.768C46.487,67.979,46.004,68.085,45.521,68.085z" fill="#45596B"></path> <path d="M45.521,84.912c-0.483,0-0.965-0.105-1.414-0.317L2.109,64.641c-1.643-0.78-2.341-2.746-1.562-4.389 c0.78-1.645,2.742-2.342,4.388-1.562l40.584,19.282L86.115,58.69c1.642-0.78,3.606-0.083,4.386,1.562 c0.78,1.643,0.083,3.608-1.56,4.389L46.934,84.595C46.487,84.807,46.004,84.912,45.521,84.912z" fill="#45596B"></path> </g> </g> </g></svg>
+			<svg width="38px" height="38px" viewBox="0 0 91 91" enable-background="new 0 0 91 91" id="Layer_1" version="1.1" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M88.587,25.783L46.581,5.826c-0.672-0.317-1.45-0.317-2.12,0L2.462,25.783 c-0.861,0.408-1.41,1.277-1.41,2.232c0,0.951,0.549,1.819,1.41,2.229l41.999,19.954c0.335,0.158,0.697,0.239,1.059,0.239 c0.363,0,0.726-0.081,1.061-0.239l42.006-19.954c0.861-0.41,1.41-1.278,1.41-2.229C89.997,27.06,89.448,26.191,88.587,25.783z" fill="#647F94"></path> <path d="M45.521,68.085c-0.483,0-0.965-0.105-1.414-0.317L2.109,47.813c-1.643-0.781-2.341-2.744-1.562-4.386 c0.78-1.642,2.742-2.341,4.388-1.562l40.584,19.283l40.595-19.283c1.639-0.78,3.606-0.083,4.386,1.562 c0.78,1.643,0.083,3.605-1.562,4.386L46.934,67.768C46.487,67.979,46.004,68.085,45.521,68.085z" fill="#45596B"></path> <path d="M45.521,84.912c-0.483,0-0.965-0.105-1.414-0.317L2.109,64.641c-1.643-0.78-2.341-2.746-1.562-4.389 c0.78-1.645,2.742-2.342,4.388-1.562l40.584,19.282L86.115,58.69c1.642-0.78,3.606-0.083,4.386,1.562 c0.78,1.643,0.083,3.608-1.56,4.389L46.934,84.595C46.487,84.807,46.004,84.912,45.521,84.912z" fill="#45596B"></path> </g> </g> </g></svg>
 			<div style={{ marginLeft: '.8em', display: 'flex', flexDirection: 'column', color: '#647F94' }}>
-				<span style={{ fontWeight: 800, lineHeight: '1em' }}>
-					myREPO.ru
+				<span style={{ fontWeight: 800, lineHeight: '1.2em' }}>
+					Мой репозиторий
 				</span>
-				<span style={{ fontWeight: 400, fontSize: '.9em', lineHeight: '1em' }}>
-					сайт-портфолио техписателя
+				<span style={{ fontWeight: 400, fontSize: '.8em', lineHeight: '1.2em' }}>
+					для поиска работы и общения
 				</span>
 			</div>
     </>
@@ -19,23 +19,40 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/pnrf/tech-docs',
   },
 	search: {
-		placeholder: 'Поиск по сайту'
+		placeholder: 'Поиск по сайту...'
 	},
-  // chat: {
-  //   link: 'https://discordapp.com/users/1058075588700672041',
-  // },
-  docsRepositoryBase: 'https://github.com/pnrf/tech-docs',
+  // docsRepositoryBase: 'https://github.com/pnrf/tech-docs',
+	sidebar: {
+		toggleButton: true
+	},
+	toc: {
+		title: "Оглавление"
+	},
+	editLink: { 
+		component: null
+	},
+	feedback: {
+		content: null
+	},
   footer: {
     text: (
       <span>
         <p>{new Date().getFullYear()} ©{' Все права защищены. '}</p>
-        <a href="https://nextra.site" target="_blank">
-          Powered by Nextra
-        </a>
-        .
+				<span>
+					Powered by <a href="https://nextra.site" target="_blank"> Nextra</a> and <a href="https://vercel.com" target="_blank">Vercel</a>.
+				</span>
       </span>
-    ),
+    )
   },
+	themeSwitch: {
+    useOptions() {
+      return {
+        light: 'Светлая тема',
+        dark: 'Темная тема',
+        system: 'Системная тема'
+      }
+    }
+  }
 }
 
 export default config
